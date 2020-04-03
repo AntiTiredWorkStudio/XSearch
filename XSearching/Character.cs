7bea343386794be25e64a381b68b29747c84c86b
konglf2112 <konglf2112@foxmail.com>
2020-04-03 15:51:47 +0800

    搜寻嫌疑人x
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

---
+++ XSearching/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XSearching
+{
+    class Character
+    {
+        /// <summary>
+        /// 身份定位
+        /// </summary>
+        public Identity identitiy;
+        /// <summary>
+        /// 社会地位
+        /// </summary>
+        public int SocialLevels;
+
+    }
+}
---
+++ XSearching/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XSearching
+{
+    /// <summary>
+    /// 身份定义 解锁进程
+    /// </summary>
+    public enum Identity
+    {
+        XMen,//关键人
+        Informant,//线人
+        Outsider//外围人
+    }
+    /// <summary>
+    /// 阶级 解锁难易度
+    /// </summary>
+    public enum SocialLevels
+    {
+        Underclass,//底层
+        Middleclass,//中产阶级
+        Eliteclass//精英阶级
+    }
+}
